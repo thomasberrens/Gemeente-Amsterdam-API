42e9df0729c4ad8860d665fd22a7755842a6298a
Thomas Berrens <thomas.berrens@me.com>
2023-05-22 13:00:31 +0200

    deleted debug

--- src/main/java/org/gemeenteamsterdam/api/WebbackendApplication.java
+++ src/main/java/org/gemeenteamsterdam/api/WebbackendApplication.java
@@ -52,13 +52,7 @@ public class WebbackendApplication {
 	CommandLineRunner initializeValuesForDatabase(UserServiceImpl userService, ApplicationConfig applicationConfig) {
 		return args -> {
 			userService.createUser(new CreateAccountDTO(applicationConfig.getAdminEmail(), applicationConfig.getAdminPassword(), "admin"));
-
-
-
 			userService.addRoleToUser(applicationConfig.getAdminEmail(), "ROLE_ADMIN");
-
-			System.out.println("APPLICATION CONFIG: " + applicationConfig);
-
 		};
 	}
 }
